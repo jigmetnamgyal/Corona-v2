657f7bbe23506071033634ba98c304ea55aa6252
jigmet12 <jigmetashi02@gmail.com>
2020-07-30 17:33:21 +0400

    made small changes

--- corona/src/Map.js
+++ corona/src/Map.js
@@ -8,12 +8,14 @@ function Map({countries, caseType, center, zoom }) {
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
           url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, caseType)}
       </LeafletMap>
     </div>
   );
 }
+ 
+
 
 export default Map;
