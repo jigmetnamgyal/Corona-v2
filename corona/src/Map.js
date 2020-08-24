d31793fe2990c47b36b25377536465aa5da41168
jigmet12 <jigmetashi02@gmail.com>
2020-08-25 01:18:55 +0400

    Small changes in the map theme ✓❉

--- corona/src/Map.js
+++ corona/src/Map.js
@@ -7,7 +7,7 @@ function Map({countries, caseType, center, zoom }) {
     <div className="mapL">
       <LeafletMap center={center} zoom={zoom}>
         <TileLayer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
           attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
         />
         {showDataOnMap(countries, caseType)}
